451610692749905d66fd7d84ecceae5b6a4efd90
Rellot's Screwdriver <Rellotscrewdriver@users.noreply.github.com>
2024-04-03 19:45:49 +0530

    made the prompt a bit prettier

--- src/toss.py
+++ src/toss.py
@@ -1,20 +1,20 @@
 # starts the game
 def startTossing():
     global plyChoice #made this variable global since determinewinner() function needs it after taking it's userinput
-    print("What do you choose? odd or even?")
+    print("\nWhat do you choose? odd or even?")
     plyChoice = input("your input: ")
     compreToss()
 
 # compares the toss and determines what computer will choose next
 def compreToss():
     if plyChoice == "odd" or plyChoice == "Odd":
-        print("computer chose even")
+        print("\ncomputer chose even")
     elif plyChoice == "even" or plyChoice == "Even":
-        print("computer chose odd")
+        print("\ncomputer chose odd")
     else:
-        print("invalid choice, try again")
+        print("\ninvalid choice, try again")
 
 def tossMatch():
-    print("draw from range 1 to 6")
+    print("\ndraw from range 1 to 6")
     draw = int(input("your input: "))
     return draw
